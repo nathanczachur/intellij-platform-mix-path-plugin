fd68194466114572eea97de0a151a9ffaaae3cc3
GitHub Action <action@github.com>
2021-12-13 21:51:21 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nathanczachur/intellijplatformmixpathplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nathanczachur.intellijplatformmixpathplugin.services
+
+import com.github.nathanczachur.intellijplatformmixpathplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/nathanczachur/intellijplatformmixpathplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nathanczachur/intellijplatformmixpathplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nathanczachur.intellijplatformmixpathplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nathanczachur.intellijplatformmixpathplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
